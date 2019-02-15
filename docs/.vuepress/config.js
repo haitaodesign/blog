bf332344695df704cd6f227941b11ae08a4fa060
haitaodesign <15393243613@163.com>
2019-02-15 17:33:45 +0800

    feat: 初始化大致知识结构

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  title: '前端知识体系搭建',
+  description: 'web前端，跨终端，小程序，node，devops',
+  head: [
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico'}]
+  ],
+  serviceWorker: true,
+  themeConfig: {
+    nav: [
+      { text: '基础', link: '/base/' },
+      { text: '进阶', link: '/advanced/' },
+      { text: '框架', link: '/framework/' },
+      { text: 'Node', link: '/node/'},
+      { text: '项目', link: '/project/' },
+      { text: '面试', link: '/interview/' },
+      { text: 'Github', link: 'https://github.com/haitaodesign/blog'}
+    ],
+    sidebar: {
+      '/components/': [
+        '',
+        'components-one',
+        'components-two'
+      ]
+    }
+  },
+  port: 8081
+}
