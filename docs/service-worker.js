a148dd5623a912b0fa9fbdbc1cac000c2a4719b3
李海涛 <liht@dustess.com>
2020-02-13 21:55:34 +0800

    chore: release

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -21,11 +21,15 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "307c985710dee7fe37235c5391a9b2ae"
+    "revision": "5e2b4093c1f0d3a22150a63d09165e79"
+  },
+  {
+    "url": "ability/index.html",
+    "revision": "9e3765be0336f71265a6591ae6fe2884"
   },
   {
     "url": "advanced/index.html",
-    "revision": "428430d824fb84d50011862185e30f08"
+    "revision": "219ae585bf37066eb7064959a2c8979b"
   },
   {
     "url": "assets/css/0.styles.8552d175.css",
@@ -36,76 +40,88 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.e5dc9f22.js",
-    "revision": "de493a69e1e70517e9cbd8d4e46c5af4"
+    "url": "assets/js/10.0f266044.js",
+    "revision": "c057a171b05f75e381ff9d9e19de187e"
   },
   {
-    "url": "assets/js/11.412d3f93.js",
-    "revision": "22cf950f6d4aede90f7b8304e8206e23"
+    "url": "assets/js/11.34844bad.js",
+    "revision": "31b93bd326e6bec017f1bbc6c6f94a2d"
   },
   {
-    "url": "assets/js/2.2ab4813c.js",
-    "revision": "bbd498f606cfd3cc8eeab1b1dbf2f4b1"
+    "url": "assets/js/12.e37a723c.js",
+    "revision": "163a1dd9baaaa36431d3949703861b35"
+  },
+  {
+    "url": "assets/js/13.bf048894.js",
+    "revision": "8b97faad765d1672d730f12da54f1b7f"
+  },
+  {
+    "url": "assets/js/2.de3881b8.js",
+    "revision": "1e7ccb25d148b4f8dcfd5111f4116f7a"
   },
   {
     "url": "assets/js/3.67dd3eab.js",
     "revision": "cf78ea4d8d29b993161d44dfc4d301ee"
   },
   {
-    "url": "assets/js/4.c1ed6e73.js",
-    "revision": "ae78b644613e4839eba0653387c2b331"
+    "url": "assets/js/4.45e127a0.js",
+    "revision": "d01b814b4000c85cd499119d876a2ef0"
   },
   {
-    "url": "assets/js/5.91f1eb7f.js",
-    "revision": "67ab5b6fd97d67a427d2a7a309735c32"
+    "url": "assets/js/5.0fc5ef47.js",
+    "revision": "fbcb68c86a492ff29cc95eaef17c0d21"
   },
   {
-    "url": "assets/js/6.8386c9e3.js",
-    "revision": "f0255dcc69afa141649eb4b18a7a31a5"
+    "url": "assets/js/6.92c07d8b.js",
+    "revision": "aaa6433c56dd421f653b615af7e25fff"
   },
   {
-    "url": "assets/js/7.04d27878.js",
-    "revision": "3a6c65206ae0180a329dd50f740e341d"
+    "url": "assets/js/7.40e78727.js",
+    "revision": "acbf31cea119006a484f08f6c12eb6c8"
   },
   {
-    "url": "assets/js/8.1ce5de35.js",
-    "revision": "3c6d76219139faf1f3c7a55fa6b047ef"
+    "url": "assets/js/8.417a172b.js",
+    "revision": "bd06c090461e13749856ebb845d52a14"
   },
   {
-    "url": "assets/js/9.fcdf2405.js",
-    "revision": "0bb076d8a5e8bed11460d5955e714a41"
+    "url": "assets/js/9.182b1bcc.js",
+    "revision": "492d4b8022508e78b457d91dda761b50"
   },
   {
-    "url": "assets/js/app.73fc69d4.js",
-    "revision": "8e0a24c4752dd7b0acaf23f38ec0c6a7"
+    "url": "assets/js/app.505ff26c.js",
+    "revision": "c3585aceb51f5fbee186148e200db3fa"
   },
   {
     "url": "base/html-meta-tag.html",
-    "revision": "1f0d325a920d23d8eac2b9dcbb01b5e2"
+    "revision": "e459e3bbba567d80172b192315fe6a33"
   },
   {
     "url": "base/index.html",
-    "revision": "52fffdff33ecafcd80666371e3835f72"
+    "revision": "b537b44bc5b27238e603c3dd7c5e7aad"
   },
   {
-    "url": "careerPlan/index.html",
-    "revision": "1747c4ce9d29c605e91afb547f6c69cf"
-  },
-  {
-    "url": "framework/index.html",
-    "revision": "6fd6f00b2fa55b7561416439884fcf94"
+    "url": "career-planing/index.html",
+    "revision": "c6b8d969910235ec4e3d4232737a30c3"
   },
   {
     "url": "index.html",
-    "revision": "9f2c8f526ff3263eb286a6e0f2da2b2c"
+    "revision": "3c261bd20c0ed844de8ba04e1238c2d5"
   },
   {
     "url": "node/index.html",
-    "revision": "fc900adeea333479e710027d3c2d4da1"
+    "revision": "84fcb80069d5705e4255984b5c5f82af"
+  },
+  {
+    "url": "practice/index.html",
+    "revision": "65f3b8fa7ee0154678fb70c812be145b"
   },
   {
     "url": "project/index.html",
-    "revision": "be6b81b46b52708cf73632cd8216f3bf"
+    "revision": "db2190fcbbdbd0c08f60ebe962f17cdd"
+  },
+  {
+    "url": "technology-system/index.html",
+    "revision": "3441e4e04f2b911982627085868aeadf"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
